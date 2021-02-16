7e68091d2294fae1894e11aaee1dbddefc28a9af
Jimmy Lee <jimmy@soomgo.com>
2021-02-16 11:25:13 +0900

    fix

--- lib/KoreaAdministrativeAreaGeoJsonUtil.ts
+++ lib/KoreaAdministrativeAreaGeoJsonUtil.ts
@@ -1,5 +1,5 @@
 import { FeatureCollection, MultiPolygon, Polygon } from "geojson";
-import { Observable, of } from "rxjs";
+import { of } from "rxjs";
 import { concatMap, map } from "rxjs/operators";
 import * as turf from "@turf/turf";
 import {
@@ -21,7 +21,7 @@ export class KoreaAdministrativeAreaGeoJsonUtil {
   private readonly admAllGeoJson: AdministrativeAreaFeatureCollection;
 
   // 행정구역 geojson 캐시 인스턴스
-  unionAdministrativeAreaFeatureCache = new UnionAdministrativeAreaFeatureCache();
+  private readonly unionAdministrativeAreaFeatureCache = new UnionAdministrativeAreaFeatureCache();
 
   constructor(
     admSdGeoJson: AdministrativeAreaFeatureCollection,
@@ -37,48 +37,50 @@ export class KoreaAdministrativeAreaGeoJsonUtil {
    * 대한민국 전체 경계 GeoJSON 조회
    */
   getKorea() {
-    return of(this.admAllGeoJson);
+    return of(this.admAllGeoJson).toPromise();
   }
 
   /**
    * 주어진 행정구역 목록을 병합한 GeoJSON 조회
    */
-  getByAdministrativeArea(
+  mergeAreas(
     administrativeAreaCodes: string[]
-  ): Observable<FeatureCollection<Polygon | MultiPolygon> | null> {
+  ): Promise<FeatureCollection<Polygon | MultiPolygon> | null> {
     // 행정구역 목록이 비어있을경우 null 반환
-    if (administrativeAreaCodes.length === 0) return of(null);
+    if (administrativeAreaCodes.length === 0) return of(null).toPromise();
 
-    return of(administrativeAreaCodes).pipe(
-      // 행정 구역 code 목록 => 행정구역 GeoJSON 목록 매핑
-      map((areaCodes) => {
-        // 시도 행정구역 GeoJSON 목록 조회
-        const sdFeatures = this.admSdGeoJson.features.filter((feature) =>
-          areaCodes.includes(feature.properties.adm_cd)
-        );
-        // 시군구 행정구역 GeoJSON 목록 조회
-        const sggFeatures = this.admSggGeoJson.features.filter((feature) =>
-          areaCodes.includes(feature.properties.adm_cd)
-        );
-        // 시도 & 시군구 행정구역 GeoJSON 목록 병합
-        return [...sdFeatures, ...sggFeatures];
-      }),
-      // 행정구역 GeoJSON 목록 => 병합된 Polygon GeoJSON 변환
-      concatMap((features) =>
-        memoizedUnionAdministrativeAreaFeature(
-          cloneMultiPolygons(features) as AdministrativeAreaFeature[],
-          this.unionAdministrativeAreaFeatureCache
+    return of(administrativeAreaCodes)
+      .pipe(
+        // 행정 구역 code 목록 => 행정구역 GeoJSON 목록 매핑
+        map((areaCodes) => {
+          // 시도 행정구역 GeoJSON 목록 조회
+          const sdFeatures = this.admSdGeoJson.features.filter((feature) =>
+            areaCodes.includes(feature.properties.adm_cd)
+          );
+          // 시군구 행정구역 GeoJSON 목록 조회
+          const sggFeatures = this.admSggGeoJson.features.filter((feature) =>
+            areaCodes.includes(feature.properties.adm_cd)
+          );
+          // 시도 & 시군구 행정구역 GeoJSON 목록 병합
+          return [...sdFeatures, ...sggFeatures];
+        }),
+        // 행정구역 GeoJSON 목록 => 병합된 Polygon GeoJSON 변환
+        concatMap((features) =>
+          memoizedUnionAdministrativeAreaFeature(
+            cloneMultiPolygons(features) as AdministrativeAreaFeature[],
+            this.unionAdministrativeAreaFeatureCache
+          )
+        ),
+        // 병합된 Polygon GeoJSON => FeatureCollection GeoJSON 매핑
+        map((unionedFeature) =>
+          unionedFeature === null
+            ? unionedFeature
+            : (turf.featureCollection([unionedFeature]) as FeatureCollection<
+                Polygon | MultiPolygon
+              >)
         )
-      ),
-      // 병합된 Polygon GeoJSON => FeatureCollection GeoJSON 매핑
-      map((unionedFeature) =>
-        unionedFeature === null
-          ? unionedFeature
-          : (turf.featureCollection([unionedFeature]) as FeatureCollection<
-              Polygon | MultiPolygon
-            >)
       )
-    );
+      .toPromise();
   }
 
   clearCache() {
